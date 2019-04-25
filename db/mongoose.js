dd03df95afd09d731673a3c9c0c0ae267ef1449a
Akash Kulkarni <akashkulkarni796@gmail.com>
2019-04-26 03:55:46 +0530

    akash

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 const uri = "mongodb+srv://user:mruaka123@cluster0-3awwl.mongodb.net/bing_bot?retryWrites=true";
 mongoose.connect(uri, {
     useNewUrlParser:true,
-    useCreateIndex:true
 }).catch((e)=>{
     console.log('Database connectivity error ',e)
 })
