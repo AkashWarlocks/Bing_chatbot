89d017c88827c7948d3dd275bac55923491c626d
Akash Kulkarni <akashkulkarni796@gmail.com>
2019-04-26 00:56:46 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+var bodyParser = require('body-parser')
+let routes = require('./routes/routes')
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.use(
+    bodyParser.json()
+)
+
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With,X-Frame-Options, Content-Type, Accept');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.use(routes)
+
+app.listen(port, () => {
+    console.log('Server is running on port' + port)
+}) 
---
+++ routes/routes.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const bodyparser = require('body-parser');
+
+app.use(
+    bodyparser.urlencoded({
+        extended: false
+    })
+);
+app.use(
+    bodyparser.json()
+);
+router.post('/Bing_bot', (req,res)=>{
+    if(req.body.intent.name === 'Students'){
+        return res.json({
+            "payload": {
+                "google": {
+                  "expectUserResponse": true,
+                  "richResponse": {
+                    "items": [
+                      {
+                        "simpleResponse": {
+                          "textToSpeech": "this is a simple response"
+                        }
+                      }
+                    ]
+                  }
+                }
+              }
+        })
+    }
+
+})
+router = express()
