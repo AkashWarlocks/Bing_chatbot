53005aa11a2d411cf6a00e3d8011edbe570ad4a4
Akash Kulkarni <akashkulkarni796@gmail.com>
2019-04-26 06:01:11 +0530

    Aksdh

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const uri = "mongodb+srv://userDB:userdb123@cluster0-3awwl.mongodb.net/bing_bot";
 mongoose.connect(uri, {
-    dbName: 'bing_bot'
+    dbName: "bing_bot"
 }).then((conn)=>{
     console.log('Conection success')
 }).catch((e)=>{
