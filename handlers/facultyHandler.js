9362ad5cac2e41f7e1856572482e32e21cb6d6fa
Akash Kulkarni <akashkulkarni796@gmail.com>
2019-04-29 02:10:14 +0530

    akash

--- handlers/facultyHandler.js
+++ handlers/facultyHandler.js
@@ -45,12 +45,11 @@ hObj.showFaculty = async(req,res)=>{
       "data": {
         "google": {
           "expectUserResponse": true,
-      
           "richResponse": {
             "items": [
               {
                 "simpleResponse": {
-                  "textToSpeech": "Okay you can view the list of issues raised.  \nClick on any issue to see the details"
+                  "textToSpeech": "Choose a item"
                 }
               }
             ]
@@ -60,14 +59,36 @@ hObj.showFaculty = async(req,res)=>{
             "data": {
               "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
               "listSelect": {
-                "title": "Issue List",
-                "items":resp,
+                "title": "Hello",
+                "items": [
+                  {
+                    "optionInfo": {
+                      "key": "first title"
+                    },
+                    "description": "first description",
+                    "image": {
+                      "url": "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
+                      "accessibilityText": "first alt"
+                    },
+                    "title": "first title"
+                  },
+                  {
+                    "optionInfo": {
+                      "key": "second"
+                    },
+                    "description": "second description",
+                    "image": {
+                      "url": "https://lh3.googleusercontent.com/Nu3a6F80WfixUqf_ec_vgXy_c0-0r4VLJRXjVFF_X_CIilEu8B9fT35qyTEj_PEsKw",
+                      "accessibilityText": "second alt"
+                    },
+                    "title": "second title"
+                  }
+                ]
               }
             }
           }
         }
-      
-      },
+      }
     })
 }
 
