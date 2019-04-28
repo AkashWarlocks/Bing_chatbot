95aa2e65078dd39628fd554e9671173b9ae8a648
Akash Kulkarni <akashkulkarni796@gmail.com>
2019-04-29 00:01:42 +0530

    akash

--- handlers/facultyHandler.js
+++ handlers/facultyHandler.js
@@ -5,66 +5,45 @@ const faculty_db = require('../db/facultyDB.json')
 
 let hObj = {}
 hObj.showFaculty = async(req,res)=>{
-    try{
-        const faculty = await Student.find()
-        console.log('Faculty : ',faculty)
-        if(faculty_db.facutydata.length ===0){
-            return res.json({
-                "fulfillmentText": "This is a text response",
-                "payload": {
-                    "google": {
-                      "expectUserResponse": true,
-                      "richResponse": {
-                        "items": [
-                          {
-                            "simpleResponse": {
-                              "textToSpeech": "No Faculty is availabe at this moment"
-                            }
-                          }
-                        ]
-                      }
+  if(faculty_db.facultydata.length ===0){
+    return res.json({
+        "fulfillmentText": "This is a text response",
+        "payload": {
+            "google": {
+              "expectUserResponse": true,
+              "richResponse": {
+                "items": [
+                  {
+                    "simpleResponse": {
+                      "textToSpeech": "No Faculty is availabe at this moment"
                     }
                   }
-            })
-        }   else  {
-          
-            return res.json({
-                "fulfillmentText": "This is a text response",
-                "payload": {
-                    "google": {
-                      "expectUserResponse": true,
-                      "richResponse": {
-                        "items": [
-                          {
-                            "simpleResponse": {
-                              "textToSpeech": "List of Faculty coming soon"
-                            }
-                          }
-                        ]
-                      }
-                    }
-                  }
-            })
-        }
-    }catch(e){
-        return res.json({
-            "fulfillmentText": "This is a text response",
-            "payload": {
-                "google": {
-                  "expectUserResponse": true,
-                  "richResponse": {
-                    "items": [
-                      {
-                        "simpleResponse": {
-                          "textToSpeech": "Error: No Faculty is availabe at this moment"
-                        }
-                      }
-                    ]
-                  }
-                }
+                ]
               }
-        })
-    }
+            }
+          }
+    })
+}   else  {
+
+    return res.json({
+        "fulfillmentText": "This is a text response",
+        "payload": {
+            "google": {
+              "expectUserResponse": true,
+              "richResponse": {
+                "items": [
+                  {
+                    "simpleResponse": {
+                      "textToSpeech": "List of Faculty coming soon"
+                    }
+                  }
+                ]
+              }
+            }
+          }
+    })
+}
+
 }
 
 module.exports = hObj
