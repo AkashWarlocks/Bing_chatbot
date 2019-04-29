592c589c2b1aba29ff158050347073190a28a037
Akash Kulkarni <akashkulkarni796@gmail.com>
2019-04-29 17:54:19 +0530

    akash

--- handlers/libraryHandler.js
+++ handlers/libraryHandler.js
@@ -2,19 +2,22 @@ const library_db = require('../db/libraryDB.json')
 let hObj={}
 
 hObj.libraryTimings = async(req,res) => {
-console.log("lib: ",req.body.result)
+//console.log("lib: ",req.body.result)
 var date = new Date(req.body.result.parameters.id)
-var day = date.getDate()
+var day = date.getDay()
 var lb_timings = {
     "day":"",
     "timings":""
 }
+console.log(day)
 for (var i = 0; i<library_db.library_data.length;i++){
+    
     if(day === library_db.library_data[i].code){
        lb_timings.day = library_db.library_data[i].day
        lb_timings.timings = library_db.library_data[i].timing
     }
 }
+console.log(lb_timings)
 return res.json({
     "speech": "Okay..!! Follow cards for more details",
     "displayText": "Okay",   
