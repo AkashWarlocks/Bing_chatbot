7bc9792ec4207b269158aa38cd31046086908fb6
Akash Kulkarni <akashkulkarni796@gmail.com>
2019-04-29 17:02:03 +0530

    akash

--- handlers/libraryHandler.js
+++ handlers/libraryHandler.js
@@ -2,7 +2,7 @@ const library_data = require('../db/libraryDB.json')
 let hObj={}
 
 hObj.libraryTimings = async(req,res) => {
-console.log("lib: ",req.body)
+console.log("lib: ",req.body.result)
 return res.json({
     speech: "Can I know your good name please ?",
         displayText: "Can I know your name ?",
