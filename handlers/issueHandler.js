805f8d2892dd27b6a83fc59053383cd84d473549
Akash Kulkarni <akashkulkarni796@gmail.com>
2019-05-02 14:06:12 +0530

    akash

--- handlers/issueHandler.js
+++ handlers/issueHandler.js
@@ -246,6 +246,7 @@ hObj.viewIssues = async(req,res)=>{
             "title": "Issue"
           })
         }
+        console.log("list of issues",resp)
         return res.json({
           "speech": "Okay I will get list of issues raised in the form of list",
           "displayText": "Now you can see all the issue raised.",
